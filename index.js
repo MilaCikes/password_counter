6896363f285ba41500a9a2c8d09a008cef3ca065
MilaCikes <138057364+MilaCikes@users.noreply.github.com>
2024-06-24 13:45:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const pass1 = document.getElementById("pass-1");
+const pass2 = document.getElementById("pass-2");
+const passBtn = document.getElementById("pass-btn");
+
+passBtn.addEventListener("click", generatePasswords);
+
+function generatePasswords() {
+  let passwordLength = 12;
+  let password1 = "";
+  let password2 = "";
+
+  for (let i = 0; i < passwordLength; i++) {
+    password1 += characters[Math.floor(Math.random() * characters.length)];
+    password2 += characters[Math.floor(Math.random() * characters.length)];
+  }
+
+  pass1.textContent = password1;
+  pass2.textContent = password2;
+}
